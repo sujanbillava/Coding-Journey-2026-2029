529c3044d025ee8c30e3a943928ab52b7eb04d26
Sujan Billava <billavasujan@gmail.com>
2026-07-17 18:12:13 +0530

    Update calculator.java

--- Day-02/calculator.java
+++ Day-02/calculator.java
@@ -1,3 +1,40 @@
-public class calculator {
-    
+import java.io.*;
+import java.util.*;
+public class calculator 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Start Your Calculation");
+        System.out.print("Enter the Number=");
+        double num1=sc.nextDouble();
+        System.out.print("Enter the operators=");
+        char operators=sc.next().charAt(0);
+        System.out.print("Enter the Number=");
+        double num2=sc.nextDouble();
+        switch(operators)
+        {
+            case '+':
+                System.out.print("Sum="+(num1+num2));
+                break;
+                
+            case '-':
+                System.out.print("Sum="+(num1-num2));
+                break;
+            case '*':
+                System.out.print("Sum="+(num1*num2));
+                break;
+            case '/':
+                System.out.println("Sum="+(num1/num2));
+                break;
+            case '%':
+                System.out.println("Sum="+(num1%num2));
+                break;
+            
+            default:
+                System.out.println("Code Run Sucessful");
+        }
+        
+
+    }    
 }
