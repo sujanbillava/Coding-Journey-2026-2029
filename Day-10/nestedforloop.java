ee8d5e465315b1baddbc091a60c6cca2b8066da1
Sujan Billava <billavasujan@gmail.com>
2026-07-20 17:01:53 +0530

    Day-10 Nested forLoop

---
+++ Day-10/nestedforloop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class nestedforloop
+{
+    public static void main(String[]args)
+    {
+        for(int i=1;i<=3;i++)
+        {
+            
+            for(int j=1;j<=3;j++)
+            {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+}
