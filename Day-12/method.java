ab109092629a75291f9d349d1148c7c04d867683
Sujan Billava <billavasujan@gmail.com>
2026-07-22 18:13:04 +0530

    Day-12 Method()

---
+++ Day-12/method.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class method 
+{
+    public static void welcome()
+    {
+        System.out.println("Welcome to My JAVA Journey");
+    }
+    public static void bye()
+    {
+        System.out.println("Thank You");
+    }
+    public static void main(String[]args)
+    {
+        welcome();
+        bye();
+    }
+}
